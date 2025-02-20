991efb4a7bd6e8065a6f90105f81a672c2634086
alemiranda <alejandroxq52@gmail.com>
2025-02-19 22:46:47 -0300

    primer pre entrega

---
+++ OneDrive/Escritorio/react-coder/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Navbar } from "./components/Navbar"
+import { ItemListConteiner } from "./components/ItemListConteiner"
+
+export default () => {
+  return (
+<>
+<Navbar />
+<ItemListConteiner nombre = "Alejandro" />
+</>
+  )
+}
+
---
+++ OneDrive/Escritorio/react-coder/src/components/ItemListConteiner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ItemListConteiner = (props) => {
+  return (
+   <div>
+    <h2>Bienvenidos {props.nombre} a Asociacion villa el carrizal</h2>
+   </div>
+  )
+}
